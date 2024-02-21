c71a0506994e0a2a88f709c5eb5b0dcd2758da06
monsieurjonees <phil.reitzjones@me.com>
2024-02-21 13:16:09 -0600

    Minimal work done

---
+++ wire.gd
@@ -0,0 +1,19 @@
+extends AnimatableBody2D
+
+var _originNode: TestNode
+var _destinationNode: TestNode
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#var originPos = _originNode.position
+	#var destPos = _destinationNode.position
+	
+	
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	pass
