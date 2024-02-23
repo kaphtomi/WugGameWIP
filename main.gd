444b974d1cc9ac967fb21a08d833002bfd67a3cf
monsieurjonees <phil.reitzjones@me.com>
2024-02-23 13:15:18 -0600

    Bunch of stuff

--- main.gd
+++ main.gd
@@ -7,18 +7,25 @@ const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var size = get_viewport().size
-	$TestNode.set_position(Vector2(rng.randi_range(0, size.x), rng.randi_range(0, size.y)))
-	$TestNode2.set_position(Vector2(rng.randi_range(0, size.x), rng.randi_range(0, size.y)))
-	$TestNode3.set_position(Vector2(rng.randi_range(0, size.x), rng.randi_range(0, size.y)))
-	$TestNode4.set_position(Vector2(rng.randi_range(0, size.x), rng.randi_range(0, size.y)))
-	$TestNode5.set_position(Vector2(rng.randi_range(0, size.x), rng.randi_range(0, size.y)))
+	#$TestNode.set_position(Vector2(rng.randi_range(0, size.x), rng.randi_range(0, size.y)))
+	#$TestNode2.set_position(Vector2(rng.randi_range(0, size.x), rng.randi_range(0, size.y)))
+	#$TestNode3.set_position(Vector2(rng.randi_range(0, size.x), rng.randi_range(0, size.y)))
+	#$TestNode4.set_position(Vector2(rng.randi_range(0, size.x), rng.randi_range(0, size.y)))
+	#$TestNode5.set_position(Vector2(rng.randi_range(0, size.x), rng.randi_range(0, size.y)))
+#
+	#$TestNode.change_text(alpha[rng.randi_range(0, 25)])
+	#$TestNode2.change_text(alpha[rng.randi_range(0, 25)])
+	#$TestNode3.change_text(alpha[rng.randi_range(0, 25)])
+	#$TestNode4.change_text(alpha[rng.randi_range(0, 25)])
+	#$TestNode5.change_text(alpha[rng.randi_range(0, 25)])
 
-	$TestNode.change_text(alpha[rng.randi_range(0, 26)])
-	$TestNode2.change_text(alpha[rng.randi_range(0, 26)])
-	$TestNode3.change_text(alpha[rng.randi_range(0, 26)])
-	$TestNode4.change_text(alpha[rng.randi_range(0, 26)])
-	$TestNode5.change_text(alpha[rng.randi_range(0, 26)])
-
+	$TestNode.change_text("A")
+	$TestNode2.change_text("J")
+	$TestNode3.change_text("Q")
+	$TestNode4.change_text("Y")
+	$TestNode5.change_text("L")
+	$TestNode6.change_text("M")
+	$TestNode7.change_text("O")
 	pass # Replace with function body.
 
 
--- wire.gd
+++ wire.gd
@@ -3,12 +3,13 @@ extends AnimatableBody2D
 var _originNode: TestNode
 var _destinationNode: TestNode
 
+var _originPoint: Vector2
+var _destPoint: Vector2
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#var originPos = _originNode.position
 	#var destPos = _destinationNode.position
-	
-	
 	
 	pass # Replace with function body.
 
