9c75453a929f4db8538d902a3a49c59bda16849d
t-o-b-i-e <tschecte@macalester.edu>
2024-02-25 18:20:45 -0600

    setting positions of wires instead of transform

--- wire.gd
+++ wire.gd
@@ -1,22 +1,27 @@
 extends AnimatableBody2D
 
-var _originNode: TestNode
-var _destinationNode: TestNode
+var _inNode: TestNode
+var _outNode: TestNode
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	
 	#var originPos = _originNode.position
 	#var destPos = _destinationNode.position
 
 	pass # Replace with function body.
 
-func set_nodes(originNode: TestNode, destNode: TestNode):
-	var targetAngle = originNode.position.angle_to_point(destNode.position)
-	var distance = originNode.position.distance_to(destNode.position)
-	var scaleVector = Vector2(distance/124, 1)
-	transform = Transform2D(targetAngle, scaleVector, 0, originNode.position)
+func set_nodes(inNode: TestNode, outNode: TestNode):
+	_inNode = inNode
+	_outNode = outNode
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	
+func _process(_delta):
+	var start = _inNode.position
+	var end = _outNode.position
+	var offset = 10*Vector2.from_angle(90+start.direction_to(end).angle())
+	$Stroke.set_point_position(0,start)
+	$Stroke.set_point_position(1,end)
+	$Hitbox.polygon = PackedVector2Array([start+offset, end + offset, end -offset, start - offset])
 	pass
