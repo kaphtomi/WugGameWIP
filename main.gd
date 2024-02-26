ce51a86e09d4a9456acc33bba96c68991e9ea64f
t-o-b-i-e <tschecte@macalester.edu>
2024-02-26 12:49:08 -0600

    played around with tweens and a shader

--- main.gd
+++ main.gd
@@ -3,6 +3,7 @@ extends Node2D
 var rng = RandomNumberGenerator.new()
 
 const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const alphabest = "eeeeeeeeeeeetttttttttaaaaaaaaooooooiiiiiiinnnnnnsssssshhhhhhrrrrrrddddllllcccuuuummmwwffggyyppbbvkjxqz"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,15 +15,25 @@ func _ready():
 	#$TestNode4.change_text(alpha[rng.randi_range(0, 25)])
 	#$TestNode5.change_text(alpha[rng.randi_range(0, 25)])
 
-	$TestNode.change_text("1")
-	$TestNode2.change_text("2")
-	$TestNode3.change_text("3")
-	$TestNode4.change_text("4")
-	$TestNode5.change_text("5")
-	$TestNode6.change_text("6")
-	$TestNode7.change_text("7")
-	$TestNode8.change_text("8")
-	$TestNode9.change_text("9")
+
+	#$TestNode.change_text("1")
+	#$TestNode2.change_text("2")
+	#$TestNode3.change_text("3")
+	#$TestNode4.change_text("4")
+	#$TestNode5.change_text("5")
+	#$TestNode6.change_text("6")
+	#$TestNode7.change_text("7")
+	#$TestNode8.change_text("8")
+	$TestNode9.change_text(alphabest[randi() % alphabest.length()])
+	$TestNode.change_text(alphabest[randi() % alphabest.length()])
+	$TestNode2.change_text(alphabest[randi() % alphabest.length()])
+	$TestNode3.change_text(alphabest[randi() % alphabest.length()])
+	$TestNode4.change_text(alphabest[randi() % alphabest.length()])
+	$TestNode5.change_text(alphabest[randi() % alphabest.length()])
+	$TestNode6.change_text(alphabest[randi() % alphabest.length()])
+	$TestNode7.change_text(alphabest[randi() % alphabest.length()])
+	$TestNode8.change_text(alphabest[randi() % alphabest.length()])
+	$TestNode9.change_text(alphabest[randi() % alphabest.length()])
 	
 	$Wire.set_nodes($TestNode, $TestNode8)
 	$Wire2.set_nodes($TestNode, $TestNode4)
@@ -32,7 +43,28 @@ func _ready():
 	$Wire6.set_nodes($TestNode9, $TestNode2)
 	$Wire7.set_nodes($TestNode6, $TestNode9)
 	$Wire8.set_nodes($TestNode8, $TestNode7)
+	pop_in()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func pop_in():
+	var vertices : Array = [$TestNode, $TestNode2, $TestNode3, $TestNode4, $TestNode5, $TestNode6, $TestNode7, $TestNode8, $TestNode9]
+	for v in vertices:
+		v.scale = Vector2.ZERO
+		
+		var tween = create_tween()
+		tween.tween_interval(.5+randf()*.5)
+		tween.tween_property(v, "scale", (1+randf()*.3)*Vector2.ONE, .2)
+		tween.tween_property(v, "scale", Vector2.ONE, randf()*.1)
+		tween.play()
+	
+	var edges : Array = [$Wire, $Wire2, $Wire3, $Wire4, $Wire5, $Wire6, $Wire7, $Wire8]
+	for v in edges:
+		v.pop_in(0)
+		
+		var tween = create_tween()
+		tween.tween_interval(.9+randf()*.3)
+		tween.tween_method(v.pop_in,0.0,1.0,.3)
+		tween.play()
--- wire.gd
+++ wire.gd
@@ -15,13 +15,24 @@ func _ready():
 func set_nodes(inNode: TestNode, outNode: TestNode):
 	_inNode = inNode
 	_outNode = outNode
+	var start = _inNode.position
+	var end = _outNode.position
+	update(start,end)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	var start = _inNode.position
-	var end = _outNode.position
+	
+	pass
+	
+func update(start: Vector2, end: Vector2):
 	var offset = 10*Vector2.from_angle(90+start.direction_to(end).angle())
 	$Stroke.set_point_position(0,start)
 	$Stroke.set_point_position(1,end)
 	$Hitbox.polygon = PackedVector2Array([start+offset, end + offset, end -offset, start - offset])
 	pass
+	
+func pop_in(t:float):
+	update(_inNode.position, _inNode.position+ t*(_outNode.position-_inNode.position))
+	
+	
