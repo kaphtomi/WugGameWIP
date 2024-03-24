d93551f5dc5c55f716e889675160b453bdacd962
Miri Leonard <123122852+MiriRL@users.noreply.github.com>
2024-03-24 15:11:03 -0500

    trying to fix the start screen naming error

--- scripts/App.gd
+++ scripts/App.gd
@@ -1,17 +1,17 @@
 extends Node
 
-const StartScreen = preload("res://scenes/StartScreen.tscn")
-const GameScreen = preload("res://scenes/GameScreen.tscn")
+const NewStartScreen = preload("res://scenes/StartScreen.tscn")
+const NewGameScreen = preload("res://scenes/GameScreen.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var start_screen : StartScreen = StartScreen.instantiate()
+	var start_screen = NewStartScreen.instantiate()
 	start_screen.start.connect(_on_start)
 	add_child(start_screen)
 
 func _on_start():
 	for child in get_children():
 		child.queue_free()
-	var game_screen = GameScreen.instantiate()
+	var game_screen = NewGameScreen.instantiate()
 	add_child(game_screen)
