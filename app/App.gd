70fd146ac675480fba63ae92f8d901a684d6480f
Miri Leonard <123122852+MiriRL@users.noreply.github.com>
2024-03-24 21:39:38 -0500

    start screen works
    It goes directly from start screen to game screen, making the "app" scene obsolete

--- app/App.gd
+++ app/App.gd
@@ -1,5 +1,7 @@
 extends Node
 
+#var GameScreen = preload("res://app/game_screen/game_screen.gd")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$StartScreen.start.connect(_on_start)
@@ -7,3 +9,5 @@ func _ready():
 func _on_start():
 	for child in get_children():
 		child.queue_free()
+	#var game_screen = GameScreen.instantiate()
+	#add_child(game_screen)
--- app/start_screen/start_screen.gd
+++ app/start_screen/start_screen.gd
@@ -6,6 +6,5 @@ func _ready():
 	pass
 
 func _on_start_button_pressed():
-	start.emit()
-	pass # Replace with function body.
-
+	#start.emit()
+	get_tree().change_scene_to_file("res://app/game_screen/game_screen.tscn")
