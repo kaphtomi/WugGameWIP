29ecec7320d9af5568ba0c00bb1ca486f0f358ea
Miri Leonard <123122852+MiriRL@users.noreply.github.com>
2024-03-24 21:16:38 -0500

    readded junction script

--- app/game_screen/circuit/junction.gd
+++ app/game_screen/circuit/junction.gd
@@ -1,11 +1,52 @@
-extends Node2D
+extends Area2D
 
+var outgoing_edges : Array
+var incoming_edges : Array
+var velocity : Vector2
+var size : Vector2
+var letter: String = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	velocity = Vector2.ZERO
 
+func _process(_delta):
+	velocity *= exp(-_delta*2)
+	
+func get_outgoing_edges():
+	return outgoing_edges
+	
+func get_incoming_edges():
+	return incoming_edges
+	
+func add_outgoing(to):
+	outgoing_edges.append(to)
+	
+func add_incoming(from):
+	incoming_edges.append(from)
+	
+func remove_outgoing(to):
+	outgoing_edges.remove_at(outgoing_edges.find(to))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func remove_incoming(from):
+	incoming_edges.remove_at(incoming_edges.find(from))
+	
+func change_text(text):
+	$Letter.text = text
+	letter = text
+
+func pop_in_edges():
+	for e in outgoing_edges:
+		pop_in_edge(e)
+	
+func pop_in_edge(e):
+	e.pop_in(0)	
+	var tween = create_tween()
+	tween.tween_method(e.pop_in,0.0,1.0,.3+.1*randf())
+	tween.play()
+	
+func force(amt : Vector2):
+	velocity += amt
+	
+func get_velocity():
+	return velocity
