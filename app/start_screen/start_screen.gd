26394870a701e1ca3e7b906cef401a73fc2197cb
Kayla Olivas <132276482+kolivas380@users.noreply.github.com>
2024-04-01 21:04:22 -0500

    Added new start screen

--- app/start_screen/start_screen.gd
+++ app/start_screen/start_screen.gd
@@ -5,6 +5,6 @@ signal start
 func _ready():
 	pass
 
-func _on_start_button_pressed():
-	#start.emit()
+
+func _on_texture_button_pressed():
 	get_tree().change_scene_to_file("res://app/game_screen/game_screen.tscn")
