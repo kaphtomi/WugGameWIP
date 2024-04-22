07a200506e5f5814d29713f6117c36b124e20f75
monsieurjonees <phil.reitzjones@me.com>
2024-04-22 13:24:54 -0500

    blue highlights not dependent on difficulty

--- app/game_screen/circuit/circuit.gd
+++ app/game_screen/circuit/circuit.gd
@@ -201,16 +201,16 @@ func highlight_wires():
 	for w in selected_junction.outgoing_edges:
 		if w == null: continue
 		if w in connecting_wires: continue
-		if GlobalVariables.cur_dif == GlobalVariables.WUG_DIFF.EASY:
-			w.highlight_blue()
-		else: w.block_decay = true
+		#if GlobalVariables.cur_dif == GlobalVariables.WUG_DIFF.EASY:
+		w.highlight_blue()
+		#else: w.block_decay = true
 		potential_wires.append(w)
 	for w in selected_junction.incoming_edges:
 		if w == null: continue
 		if w in connecting_wires: continue
-		if GlobalVariables.cur_dif == GlobalVariables.WUG_DIFF.EASY:
-			w.highlight_blue(true)
-		else: w.block_decay = true
+		#if GlobalVariables.cur_dif == GlobalVariables.WUG_DIFF.EASY:
+		w.highlight_blue(true)
+		#else: w.block_decay = true
 		potential_wires.append(w)
 
 func flash_all_red():
