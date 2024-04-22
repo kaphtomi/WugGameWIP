0d39232d18d5d6ea1c2879335f72398cbe5e42e0
Kayla Olivas <132276482+kolivas380@users.noreply.github.com>
2024-04-21 22:15:27 -0500

    Working on speech

--- speechBubble.gd
+++ speechBubble.gd
@@ -5,9 +5,16 @@ var bubble_text_length = 0
 var bubble_text_index = 0
 var current_text = ""
 
-#onread
+onready var lbltext = get_node("VBoxContainer/Label")
+onready var ninerect = get_node("VBoxContainer/Label/NinePatchRect")
+
+onready var timer = get_node("Timer")
+
+onready var do_close = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	bubble_text_length = bubble_text.length()
+	timer.start(1)
 	pass # Replace with function body.
 
 
