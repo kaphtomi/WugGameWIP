956ca756bede747dfdc3cd1c0632aae2a10d7c41
Kayla Olivas <132276482+kolivas380@users.noreply.github.com>
2024-04-24 13:48:14 -0500

    More additions to bubble

--- app/start_screen/start_screen.gd
+++ app/start_screen/start_screen.gd
@@ -5,10 +5,8 @@ signal start
 func _ready():
 	pass
 
-
 func _on_texture_button_pressed():
 	get_tree().change_scene_to_file("res://app/game_screen/game_screen.tscn")
-
 
 func _on_settings_button_pressed():
 	get_tree().change_scene_to_file("res://app/settings_screen.tscn")
