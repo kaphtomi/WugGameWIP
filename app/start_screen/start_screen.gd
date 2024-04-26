0819dd24e766a53c857ea2f3fcf3b078054d08a3
monsieurjonees <phil.reitzjones@me.com>
2024-04-26 14:12:11 -0500

    Score screen underway (not finished)

--- app/start_screen/start_screen.gd
+++ app/start_screen/start_screen.gd
@@ -11,3 +11,5 @@ func _on_texture_button_pressed():
 func _on_settings_button_pressed():
 	get_tree().change_scene_to_file("res://app/settings_screen.tscn")
 
+func _on_score_button_pressed():
+	pass # Replace with function body.
