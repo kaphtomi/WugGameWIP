5203c48c9ac1af617535075fc828adc12ba917db
Kayla Olivas <132276482+kolivas380@users.noreply.github.com>
2024-04-25 20:27:10 -0500

    Trying to add message upon score

--- app/game_screen/game_screen.gd
+++ app/game_screen/game_screen.gd
@@ -61,7 +61,8 @@ func change_state():
 	if score/1000!=0:
 		GlobalVariables.cur_dif=GlobalVariables.WUG_DIFF.HARD
 		
-	#if score == 10:
-		#$SpeechBubble/VBoxContainer/Label/NinePatchRect.update_word("I'm getting sleepy... Zzzz")
+func sleepyWarning(new_text):
+	if score == 10:
+		$SpeechBubble/VBoxContainer/Label/NinePatchRect.update_word("I'm getting sleepy... Zzzz")
 		
 
