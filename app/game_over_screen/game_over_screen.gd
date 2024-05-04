f8e1f6123ff0fc74c46129086768398ca111d910
Miri Leonard <123122852+MiriRL@users.noreply.github.com>
2024-05-03 22:13:39 -0500

    just in case...

--- app/game_over_screen/game_over_screen.gd
+++ app/game_over_screen/game_over_screen.gd
@@ -3,6 +3,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$UIOrganizer/VOrganizer/ScoreLabel.text = "Score: " + str(GlobalVariables.score)
+	GlobalVariables.switch_to_awake()
 
 
 func _on_restart_button_pressed():
